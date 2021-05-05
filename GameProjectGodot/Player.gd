0484aea083243005ae335c241c06279765667711
Sammal34 <Sammal34@kingshigh.school.nz>
2021-05-05 14:53:21 +1200

    05/05/21
    tried more on movement have bug, trolled keegan today, TROLLGANG OUT

--- GameProjectGodot/Player.gd
+++ GameProjectGodot/Player.gd
@@ -30,5 +30,33 @@ func _physics_process(delta):
 	var dir = Vector3() 
 	
 	if(Input.is_action_pressed("move_fw")):
-		
+		dir += -camera.Basis[2]
+	if(Input.is_action_pressed("move_bw")):
+		dir += camera.Basis[2]
+	if(Input.is_action_pressed("move_l")):
+		dir += -camera.Basis[0]
+	if(Input.is_action_pressed("move_r")):
+		dir += camera.Basis[0]
+
+	dir.y = 0
+	dir = dir.normalized()
+	
+	velocity.y += delta * gravity
+	
+	var hv = velocity
+	hv.y = 0
+	
+	var new_pos = dir * SPEED
+	var accel = DE_ACCELERATION
+	
+	if (dir.dot(hv) > 0):
+		accel = ACCELERATION
+	
+	hv = hv.linear_interpolate(new_pos, accel * delta)
+	velocity.x = hv.x 
+	velocity.z = hv.z
+	
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
+	
+	
 	
