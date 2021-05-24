6c55f14a6cd8541355e094b55c1f72fba0a2deff
Sammal34 <Sammal34@kingshigh.school.nz>
2021-05-25 09:56:41 +1200

    25/05/21
    I continued on bullets and etc. need to keep going on stuff

--- GameProjectGodot/Bullet.gd
+++ GameProjectGodot/Bullet.gd
@@ -1,16 +1,22 @@
 extends RigidBody
 
 var shoot = false
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+const DAMAGE = 50
+const SPEED = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_as_toplevel(true)
+	
 
+func _physics_process(delta):
+	if shoot:
+		apply_impulse(transform.basis.z, -transform.basis.z)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Area_body_entered(body):
+	if body.is_in_group("Enemy"):
+		body.health -= DAMAGE
+		queue_free()
+	else:
+		queue_free()
+		
+	
