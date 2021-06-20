9e9c1d8686c82ba73cfc411d5c8c2b57a16ee110
Sammal34 <Sammal34@kingshigh.school.nz>
2021-06-21 09:42:55 +1200

    21/06/21
    fortnite making enemy drip >:)

--- GameProjectGodot/Enemy.gd
+++ GameProjectGodot/Enemy.gd
@@ -4,8 +4,19 @@ onready var nav = $"../Navigation" as Navigation
 onready var player = $"../FPS" as KinematicBody
 
 var path = []
+var current_node = 0
+var speed = 2
+
 
 func _ready():
-
 	path = nav.get_simple_path(global_transform.origin, player.global_transform.origin)
-	print(path)
+#	print(path)
+
+func _physics_process(delta):
+	if current_node < path.size():
+		var direction: Vector3 = path[current_node] - global_transform.origin
+		if direction.length() < 1:
+			current_node += 1
+		else:
+			print("Dir: ", direction)
+			move_and_slide(direction.normalized() * speed)
