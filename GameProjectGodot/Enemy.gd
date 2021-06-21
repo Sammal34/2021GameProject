4838bf4e0476e4347b1555a00b3eac3485e33e1b
Sammal34 <Sammal34@kingshigh.school.nz>
2021-06-22 10:53:29 +1200

    22/06/21
    Made the enemy move and it was drip check fortnite

--- GameProjectGodot/Enemy.gd
+++ GameProjectGodot/Enemy.gd
@@ -9,14 +9,29 @@ var speed = 2
 
 
 func _ready():
-	path = nav.get_simple_path(global_transform.origin, player.global_transform.origin)
+#	print(nav)
 #	print(path)
+	pass
 
 func _physics_process(delta):
+	
+#	if aimcast.is_colliding("Enemy.tscn"):
+#		queue_free()
+	
 	if current_node < path.size():
 		var direction: Vector3 = path[current_node] - global_transform.origin
 		if direction.length() < 1:
 			current_node += 1
 		else:
-			print("Dir: ", direction)
+#			print("Dir: ", direction)
 			move_and_slide(direction.normalized() * speed)
+
+func update_path(target_position):
+	path = nav.get_simple_path(global_transform.origin, target_position)
+	
+	
+	
+
+func _on_Timer_timeout():
+	print("lookin for da playa")
+	update_path(player.global_transform.origin)
